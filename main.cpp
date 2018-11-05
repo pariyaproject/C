1e6e7f78633ffcbdfb19fb7e53b62c3d1505c230
Sirokuma <sirokuma@irisu.cc>
2018-11-05 16:19:49 +0800

    11.5-2

--- main.cpp
+++ main.cpp
@@ -49,6 +49,34 @@ void use_bangumi_class() {
 	std::cout << "subject+=:\n" << my_subject << std::endl;
 		
 }
+//c++是一种静态数据类型语言 区别与php等运行时检查数据变量的类型
+//32位系统中一个字(word)有32bit即4个字节(bytes)
+//->32位有2^32(字节)bytes寻址空间4*2^10*2^10*2^10=4G
+//char != signed char (0~255)由编译器决定 [unsigned char(-128~127)]
+//->因为以上原因不要使用char做运算 而代替为有(un)signed修饰的
+//long 一般和 int 有一样的尺寸 longlong更好
+//不可能是负 则用unsigned修饰(大于等于0) 只有unsigned则默认unsigned int
+//给一个无符号的数赋值超范围的数结果取模后所得余数 如 unsigned char x=-1 (实际255)
+//给一个有符号的数赋值超范围的数结果是未定义
+//字符串("")比字符('')多一个空字符('\0')，两个字体串仅间隔空格，缩进，换行的话
+//则实际它们是一个整体“x” “y”=="xy"
+//指定字面值类型
+//->L'a' 宽字符字面值 类型是wchar_t [字符的是前缀]
+//->u8"hi!" utf-8字符串字面值  [数字的是后缀]
+//->nullptr是指针字面值
+//C++11新特性: int units_sold{0} 使用花括号初始化变量(列表初始化)
+//->[内置类型的变量可能的信息丢失时编译报错]
+//默认初始化，内置类型其值由定义的位置决定，定义在任何函数体外的的变量初始化为0
+//->定义在函数体内的变量不被初始化(未定义)
+//声明和定义
+//->声明时可能也定义(申请空间，赋初始)了这个变量名，可以添加extern关键字不显示地初始化变量
+//->例如：extern int i;  声明而非定义
+//->int j;  声明并定义
+//->extern double pi = 3.1416;  定义
+//->在函数体内部(当然也算main)若初始化一个extern标记的变量则错误
+void test_extern() {
+	//extern double pi = 3.1416;
+}
 int main() {
 	//输入输出
 	//cin_cout_cerr_clog();
@@ -60,7 +88,8 @@ int main() {
 	//undefine_num_for();
 	//类的创建使用
 	//use_bangumi_class();
-
+	//extern double p;
+	p = 3.1416;
 	system("pause");
 	return 0;
 }
