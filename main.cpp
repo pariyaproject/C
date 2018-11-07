84fc9b5c525fce61a680dc0b45d013b6fd8238ce
Sirokuma <sirokuma@irisu.cc>
2018-11-07 16:31:16 +0800

    11.7

--- main.cpp
+++ main.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <string>
+#include <cctype>
+#include <vector>
 //包含来自标准库的头文件时使用<>否则""
 //class
 #include "bangumi_subject.h"
@@ -38,16 +41,16 @@ void use_bangumi_class() {
 	Bangumi_subject my_subject;
 	Bangumi_subject my_subject1(123, 9, 5);
 	Bangumi_subject my_subject2(123, 8, 3);
-	Bangumi_subject my_subject3(0,0,1000);
+	Bangumi_subject my_subject3(0, 0, 1000);
 
 	std::cin >> my_subject;
-	std::cout <<"current subject:\n"<< my_subject << std::endl;
-	std::cout <<"++subject\n"<< ++my_subject << std::endl;
-	std::cout <<"subject++\n"<< my_subject++ << std::endl;
-	std::cout <<"subject+subject:\n"<< my_subject1 + my_subject2 << std::endl;
+	std::cout << "current subject:\n" << my_subject << std::endl;
+	std::cout << "++subject\n" << ++my_subject << std::endl;
+	std::cout << "subject++\n" << my_subject++ << std::endl;
+	std::cout << "subject+subject:\n" << my_subject1 + my_subject2 << std::endl;
 	my_subject += my_subject3;
 	std::cout << "subject+=:\n" << my_subject << std::endl;
-		
+
 }
 //c++是一种静态数据类型语言 区别与php等运行时检查数据变量的类型
 //->静态类型语言：在编译阶段检查类型(类型检查)
@@ -217,59 +220,285 @@ void const_test_others() {
 	constexpr const int *cpp3 = nullptr; //指向常量的常量指针
 }
 //类型别名
-//定义类型别名 传统方法
-typedef double wages;
-typedef wages base, *f; //f是 double*的同义词
-const f double_f = 0;//double_f是指向double的常量指针
-//此时的关联关系和以前的不同 f类型已经是指针了 const f 已经是基本数据类型了 double_f 才是声明符
-//c++ 11新的方法 别名声明 注意声明后才能使用
-using B_S = Bangumi_subject; //B_S是类的同义词
-using ptr_int = int *;
+void test_type() {
+	//定义类型别名 传统方法
+	typedef double wages;
+	typedef wages base, *f; //f是 double*的同义词
+	const f double_f = 0;//double_f是指向double的常量指针
+	//此时的关联关系和以前的不同 f类型已经是指针了 const f 已经是基本数据类型了 double_f 才是声明符
+	//c++ 11新的方法 别名声明 注意声明后才能使用
+	using B_S = Bangumi_subject; //B_S是类的同义词
+	using ptr_int = int *;
 
-//auto类型说明符 c++ 11
-//auto让编译器通过初始值来推算变量的类型，auto定义的变量必须有初始值
-double val1 = 1.5;
-int val2 = 5;
-auto item = val1 + val2; //double类型 
-B_S subject_1(0,0,2);
-auto subject_item = subject_1 + subject_1;//auto也可以是一个类B_S
-//虽然有auto的支持 但仍不能改变c++是强类型语言的事实
-//auto可以一次多个声明，但多个名字必须是同一个类型
-//auto sz = 0, pi = 3.14; //error
-//auto会忽略顶层const 否则显示声明 const auto
-//decltype类型指示符 c++ 11
-//作用是希望得知类型 但不不需要用其值初始化
-//返回操作数的数据类型
-//decltype(f()) sum = x; //sum的类型是函数f的返回类型
-//如果使用的是变量，则decltype返回该变量的类型(包括顶层const和引用在内) 直接相当于替换
-//如果是表达式 其中返回是引用 应当能作为左值
-//int iii = 45, *ppp = &iii, &rrr = iii;
-//decltype(rrr + 0) bbdsfsa; //r+0 =>int
-//decltype(*ppp) dfadadf; //错误 c 是Int& 类型 必须初始化 
-//int i;
-//decltype ((i)) d; //d是int & 错误 没初始化 ()当作是表达式处理 可以直接处理因此是引用
-//双层括号永远是引用 单层只有当变量本身是引用才是引用
-//decltype (i) d; //d是int 
+	//auto类型说明符 c++ 11
+	//auto让编译器通过初始值来推算变量的类型，auto定义的变量必须有初始值
+	double val1 = 1.5;
+	int val2 = 5;
+	auto item = val1 + val2; //double类型 
+	B_S subject_1(0, 0, 2);
+	auto subject_item = subject_1 + subject_1;//auto也可以是一个类B_S
+	std::cout << sizeof(item) << "  " << item << std::endl;
+	std::cout << sizeof(subject_item) << "  " << subject_item << std::endl;
+	//虽然有auto的支持 但仍不能改变c++是强类型语言的事实
+	//auto可以一次多个声明，但多个名字必须是同一个类型
+	//auto sz = 0, pi = 3.14; //error
+	//auto会忽略顶层const 否则显示声明 const auto
+	//decltype类型指示符 c++ 11
+	//作用是希望得知类型 但不不需要用其值初始化
+	//返回操作数的数据类型
+	//decltype(f()) sum = x; //sum的类型是函数f的返回类型
+	//如果使用的是变量，则decltype返回该变量的类型(包括顶层const和引用在内) 直接相当于替换
+	//如果是表达式 其中返回是引用 应当能作为左值
+	//int iii = 45, *ppp = &iii, &rrr = iii;
+	//decltype(rrr + 0) bbdsfsa; //r+0 =>int
+	//decltype(*ppp) dfadadf; //错误 c 是Int& 类型 必须初始化 
+	//int i;
+	//decltype ((i)) d; //d是int & 错误 没初始化 ()当作是表达式处理 可以直接处理因此是引用
+	//双层括号永远是引用 单层只有当变量本身是引用才是引用
+	//decltype (i) d; //d是int 
+}
 //结构体
-struct Bangumi_date {
-	//Bangumi_date() {}
-	//Bangumi_date(unsigned i,std::string n):id(i),name(n){}
-	unsigned id = 0;
+struct Bangumi_data {
+	//Bangumi_data() {}
+	//Bangumi_data(unsigned i,std::string n):id(i),name(n){}
+	//unsigned id = 0;
+	unsigned id;
 	std::string name;
-	
+	friend std::ostream& operator<< (std::ostream &os, const Bangumi_data& bd) {
+		std::cout << "name: " << bd.name << "\nid: " << bd.id << std::endl;
+		return os;
+	}
 }test_struct_0;//此外必须有; 因为类体后可以紧跟变量名
-using B_D = Bangumi_date; //或者直接加在声明前 但就不能紧跟变量名
-B_D test_struct_1;
-Bangumi_date test_struct_2;
-//c++ 11
-//可以为类内数据成员提供一个类内初始值
-//注意类内初始值 放在花括号里或等号右 不能使用圆括号
-//Bangumi_date test_struct_3{1,""}; //注意此时这里报错，只有当定义时没有默认时才正确 （unsigned id;）
-//Bangumi_date test_struct_4={1,""};//注意此时这里报错，只有当定义时没有默认时才正确 （unsigned id;）
+void test_struct() {
+	using B_D = Bangumi_data; //或者直接加在声明前 但就不能紧跟变量名
+	B_D test_struct_1;
+	Bangumi_data test_struct_2;
+	//c++ 11
+	//可以为类内数据成员提供一个类内初始值
+	//注意类内初始值 放在花括号里或等号右 不能使用圆括号
+	Bangumi_data test_struct_3{ 1,"" }; //注意此时这里报错，只有当定义时没有默认时才正确 （unsigned id;）
+	Bangumi_data test_struct_4 = { 1,"" };//注意此时这里报错，只有当定义时没有默认时才正确 （unsigned id;）
+}
 //确保多次包含头文件也能安全工作的技术是预处理器
 //预处理功能 #include 用指定的头文件代替
 //预处理变量有两个状态 已定义和未定义 #define 
-//
+//string和vector是两个重要的标准库类型
+//前者支持可变长的字符串，后者支持可变长的集合
+//内置数组是一种更基础的类型，string和vector都是对它的某种抽象
+//命名空间using声明
+void test_using() {
+	//cout << "ddd";//报错: 应当位于using之后
+	using std::cout;
+	cout << "yes";
+	using std::cin; using std::endl;
+	//using也是在其声明的作用域中有效,并且在声明语句之后
+}
+//头文件不应包含using声明,否则包含了这个头文件的文件都会有这个声明
+//标准库string
+
+void test_string() {
+	using std::string;
+	using std::cout;
+	using std::cin;
+	using std::endl;
+	//string的初始化方法
+	string s1; //空字符
+	string s2 = s1; //(拷贝初始化)s1的副本 等价于 (直接初始化)string s2(s1);
+	string s3 = "hiya"; //字面值 该字面值除了最后的空字符'\0'其它所有的字符被拷贝
+	//也就是说string这个类中没有字符串字面值的最后的'\0'
+	//->等价 s3("hiya")
+	string s4(10, 'c'); //s4的内容是 cccccccccc
+	//s4对应的拷贝初始化 string s4=string(10, 'c');
+	//初始化的方式
+	//如果使用等号 = 初始化则是拷贝初始化
+	//如果不使用等号 = 则是 直接初始化
+	//>>输入流以空白字符分隔 
+	//注意使用getline一定要包含<string> 仅仅使用string可不用
+	//getline和cin一样会返回流参数 例如 while(getline(std::cin,s1));
+	getline(std::cin, s1);//从输入流读取一行赋值给s1 相比cin>>s1只忽略空白字符,但换行依旧不存储(但有读入标志结束)
+	//如果一开始就是换行符那么将会是一个空字符
+	//s1 = { '\0' };
+	if (!s1.empty())//注意 空格 \0不是一个空字符()
+		cout << "getline=" + s1 + "=end" << endl;
+	//但是cin>>s1就不会以换行符结束,也就是说cin>>不可能出现一个空字符,必须录入一个非空白字符才会算完成这个操作
+	cin >> s1;
+	cout << s1 << endl;
+	cout << s1.empty() << endl;
+	//"xxx"这是一个C风格的字符串 是以\0为末尾识别的
+	//string 是一个对象 也有长度记录 完全可以不用\0来确定长度(什么时候结束)
+	string test1("123\0");//"123\0"就已经确定了是 '1''2''3'三个字符的赋值
+	string test2("123");
+	string test3(4, '\0');//string对象不会将\0忽略因为\0本身就是空字符的含义
+	string test4{ '1','2','3','\0' };
+	//size()返回的是string::size_type类型(无符号类型的值)因此不应混用int(至少用unsigned)
+	//auto len = s1.size();
+	//string::size_type x;
+	cout << s1.size() << endl;
+	cout << test1 << " " << test1.size() << endl;//size返回string中字符的个数
+	cout << test2 << " " << test2.size() << endl;
+	cout << test3 << " " << test3.size() << endl;
+	cout << test4 << " " << test4.size() << endl;
+	//但是使用 c_str() data()将string对象转换为const char *时最后一个字符是'\0'
+	string blan;
+	cout << "空白=" << blan[blan.size()] << "=ddd" << endl;
+	cout << "空白=" << test1[test1.size()] << "=ddd" << endl;//0-size()-1是存储有效字符
+	//当试图访问size()位时会固定返回'\0'
+	//reference:https://segmentfault.com/q/1010000005141633
+	//==如果s1 s2所含的字符完全一样才相等(大小写敏感)
+	//>= <= 根据在字典中的顺序对比 
+	string a("a");
+	string b("b");
+	cout << (a >= b) << endl;// a<b 依此类推 
+	//string("a")<string("aa")
+	//如果两个string长度不同,并且较短的string中每个字符都与较长的对应位置相同,则较短的<较长的
+	//如果比较的字符在某些位置不同,结果是第一对不同的字符的字典顺序
+	//string 对象可以使用+ 返回是一个新的string对象(所有字符拼接)
+	//但是必须至少有一个对象是string(并且必须先进行string和字面值的运算)不能是字符串字面值: "123"+"dd"
+	string("123") + "123" + "222";
+	//"123" + "   " + string("ddd");//非法存在字面值直接相加的过程
+	//位于 <cctype>
+	isalnum('s');//当字母或数字时为真
+	//isalpha > 字母
+	//iscntrl > 控制字符
+	//isdigit > 数字
+	//islower > 小写字母
+	//isprint > 可打印
+	//ispunct > 标点符号
+	//tolower > 输出小写 注意返回的是int类型
+	//toupper > 输出大写 注意返回的是int类型
+
+	//基于范围的for  range for
+	//for (declaration : expression) 遍历给定序列的每个元素
+	//每次迭代declaration会被初始化为expression部分的下一个元素值
+	//因为string表示一个字符的序列
+	string test_range_for("aabbcc dd");
+	for (auto &each : test_range_for) {//如果需要改变值 则使用引用
+		//cout << static_cast<char>(toupper(each)) << endl;
+		each = static_cast<char>(toupper(each));
+	}
+	cout << test_range_for << endl;
+	//下标运算符[] 接受的参数是string::size_type 返回值是此位置字符的引用
+	//&& 逻辑与运算符 c++中只有左侧的运算为真才会检查右侧的运算对象
+}
+//vector 容器
+void test_vector() {
+	using std::string;
+	using std::endl;
+	using std::cout;
+	//vector表示对象的集合,其中所有对象的类型都相同 
+	//使用vector必须包含 <vector>
+	//vector也位于std命名空间 vector是一种类模板 需要提供额外的信息(类型)
+	//编译器根据模板创建类或函数的过程称为实例化
+	using std::vector;
+	vector<int> ivec; //<int>是提供的额外信息 //空vector 执行默认初始化
+	vector<Bangumi_data> bdvec;
+	vector<vector<string>> strvv;//vector可以嵌套 类似二维的数组
+	//vector<int&>d; 错误
+	//请注意 引用不是一个对象 不能放在vector中
+	//c++旧版本嵌套vector外层vector对象的右尖括号和元素类型之间要有一个空格 vector<vector<int> >
+	//c++ 11 无需空格
+	//初始化vector
+	vector<int> ivec2(ivec); //直接初始化 ivec的副本
+	vector<int> ivec3 = ivec; //赋值初始化 等价上一个(ivec)
+	vector<int> ivec4(10, 1); //包含了10个值为1的int
+	//vector<int> ivec44 = (10, 1); //错误 
+	vector<int> ivec5(11); //包含了11个执行了默认初始化的int
+	vector<int> ivec6{ 1,2 }; //两个元素 分别是1 2
+	//c++11 提供了列表初始化 vector
+	vector<int> ivec7 = { 1,2 };//可以看作 vector<int> ivec6={int(1),int(2)}
+	vector<string> strvec = { "dd","ddd" };
+	//vector<Bangumi_data> bdvec2 = { {1,""},{2,"ddd"} }; //可以初始化列表中使用初始化列表
+	vector<Bangumi_data> bdvec2 = { { 1,"" },{2,"ddd"} };//不同于类 即使没有相关的构造函数依旧可以直接使用{}初始化
+	Bangumi_subject test_bs1 = { 1,2,3 }; //类也可使用初始化列表的形式,但实际上是调用了相关的构造方法
+	//->正如之前所提到的使用{}列表初始化编译会检查类型,不对便会报错 
+	cout << "BangumiSubject" << test_bs1 << endl;
+	vector<Bangumi_subject> bsvec = { {1,2,3},{2,3,4} };//不能使用{(1,2,3)} 只能用列表初始化
+	//综上 vector中的初始化列表[严格要求类型一致]
+	//->隐藏了对应的对象类型名 如 Bangumi_subject{1,2,3} => {1,2,3} int{1}=>1
+	for (auto bd : bdvec2) {
+		cout << bd << endl;
+	}
+	for (auto bs : bsvec) {
+		cout << bs << endl;
+	}
+	//圆括号用来构造 花括号用来列表初始化
+	ivec.push_back(2); //向vector尾添加元素
+	//vector是可以高效地增长的,只有一种例外初始化相同的元素
+	//否则使用运行时push新元素到一个空vector更高效
+	//为了高效很多检查保护省略,应确保无误
+	//如果循环体包含有向vector添加元素语句 时 不能使用range for
+	//除了push_back()
+	//empty() //是否存在一个元素
+	//size() //元素个数
+	//[n] //返回引用
+	//== //当且仅当元素个数相同并且对应位置值相同
+	//< > //以字典顺序比较
+	//size_type要对应
+	vector<int>::size_type testx = 0;
+	//如两个vector比较如string一样 看个数和内容
+	//内容的比较看具体的类型的比较
+	//->前提是只有vector内的元素值可比时 对应的vector才可比
+	//两个整数相除结果还是整数,余数被忽略
+	//iterator迭代器
+	//除了vector还有其他容器
+	//所有标准库容器都能使用迭代器 string也可以 
+	//迭代器有 有效和无效之分
+	//迭代器的end指向的是容器的一个本不存在的尾后,仅仅标识最后
+	//如果容器为空 则 begin 和 end 返回的是同一个迭代器 都是尾后迭代器
+	//迭代器运算符
+	//*iter 返回元素引用
+	//iter->name
+	//++iter
+	//--iter
+	//iter1 == iter2
+	string strtest("aabbccdd");
+	for (auto it = strtest.begin(); it != strtest.end(); ++it) {
+		*it = toupper(*it);
+	}
+	cout << strtest << endl;
+	//
+	string::iterator it2; //能读写字符
+	//如果vector 或 string是常量 只能 用const_iterator
+	string::const_iterator it3; //只能读取字符 不能写字符
+	//begin和end返回的具体的类型由对象是否是常量决定
+	//为了便于得到const_iterator类型
+	//c++引入了 cbegin和 cend
+	string::const_iterator testc = strtest.cbegin();
+	//某些对vector对象的操作会使迭代器失效
+	//比如在使用了迭代器的for中用使用push_back
+	//因此push_back应当在一般循环中使用
+	//迭代器运算
+	//iter + n //向前移动n个元素
+	//iter - n //向后移动
+	//iter += n
+	//iter1 - iter2 //两个迭代器之间的距离 中间的元素差个数 
+	//< > //
+	//begin()是指0下标
+	auto mid = strtest.cbegin() + strtest.size()/2;
+	string::difference_type difference = mid - strtest.cbegin(); //带符号的整形
+	//二分
+
+}
+//数组大小确定不变
+void test_array() {
+	using std::cout;
+	using std::endl;
+	//数组a[b]的维度(b)必须是常量表达式
+	unsigned cnt = 45; //不是常量表达式
+	//int arr[cnt]; //错误
+	constexpr unsigned cnt1 = 45;
+	int arr[cnt1]; //可以
+	int arr2[sizeof(int)]; //可以
+	//和内置类型一样 在函数体内定义了某种类型的数组,默认初始化会含有未定义的值
+	//定义数组必须指定数组的类型 不能用auto
+	//auto xxx[] = { 1,1,1 };//报错
+	//数组的元素为对象 不存在引用 
+	//显示初始化数组元素(列表初始化)
+	int a3[5] = { 0,1,2 }; //等价 a3[]={0,1,2,0,0} 函数体外否则未定义
+	//cout << a3[4] << "  " << a3[5] << endl; //未定义值
+}
+
+
 int main() {
 	//输入输出
 	//cin_cout_cerr_clog();
@@ -285,10 +514,13 @@ int main() {
 	//p = 3.1416;
 	//指针
 	//test_pointer();
-	std::cout << sizeof(item)<<"  "<<item<<std::endl;
-	std::cout << sizeof(subject_item)<<"  "<<subject_item<<std::endl;
 	//extern const关键字
 	//std::cout << x;
+	//test_using();
+	//cout << "end"; //using在块位域中声明只在其中有效
+	//test_string();
+	//test_vector();
+	test_array();
 	system("pause");
 	return 0;
 }
